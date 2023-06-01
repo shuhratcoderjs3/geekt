f00c78b487190267e912b8c9e6395d5c2da35ee3
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-06-01 11:38:29 +0500

    hero don

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <>
+      <section className='hero-section'>
+        <div className="container">
+          <div className='hero-section-box'>
+            <h1 className='hero-section-title'>Решения для бизнеса</h1>
+            <p className='hero-section-subtitle'>
+            Это текст. Нажмите здесь, чтобы изменить его.​
+            </p>
+            <button className="site-header-btn">Позвонить</button>
+          </div>
+        </div>
+     </section>
+    </>
+  )
+}
+
+export default Hero;
